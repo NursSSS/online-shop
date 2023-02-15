8d75ceff5a30c30b5df4801a8c0cd26571224f7d
Адиль Кашиев <adilkashiev@gmail.com>
2023-02-15 21:56:18 +0600

    fixed news feature

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,24 +1,47 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { NewsService } from './news.service';
 import { AddNews } from './dto/add-news.dto';
-import { DeleteNews } from './dto/delete-news.dto';
+import { RoleGuard } from 'src/auth/guard/role-guard';
+import { Role } from 'src/user/enum/role-decorator';
+import { UserRole } from 'src/user/enum/role.enum';
 
 @Controller('news')
 export class NewsController {
   constructor(private newsService: NewsService) {}
 
+  @Role(UserRole.ADMIN)
+  @UseGuards(RoleGuard)
   @Get()
   async getNews() {
     return await this.newsService.getNews();
   }
 
+  @Role(UserRole.ADMIN)
+  @UseGuards(RoleGuard)
   @Post('add')
   async addNews(@Body() newsDto: AddNews) {
     return await this.newsService.addNews(newsDto);
   }
 
-  @Delete('delete')
-  async deleteNews(@Body() delNewsDto: DeleteNews) {
-    return this.newsService.deleteNewByID(delNewsDto);
+  @Role(UserRole.ADMIN)
+  @UseGuards(RoleGuard)
+  @Delete('delete/:id')
+  async deleteNews(@Param('id') id: string) {
+    return this.newsService.deleteNewsById(id);
+  }
+
+  @Role(UserRole.ADMIN)
+  @UseGuards(RoleGuard)
+  @Get(':id')
+  async getNewsById(@Param('id') id: string) {
+    return this.newsService.getNewsById(id);
   }
 }
